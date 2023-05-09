d3dbf10dfe4dbd2ce89beac113709f89dd7c2c27
ZweNN32 <132755287+ZweNN32@users.noreply.github.com>
2023-05-09 09:38:23 +0000

    webpages added

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/writer')
+def writer():
+    return render_template('writer.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.python39Packages.pip
     pkgs.python39Packages.flask
     pkgs.python39Packages.flask-cors
   ];
